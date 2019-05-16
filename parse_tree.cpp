68031359581946a7384aca58aec79a976054f40c
jzy1999 <1148464672@qq.com>
2019-05-16 16:37:26 +0800

    commit

---
+++ parse_tree.cpp
@@ -0,0 +1,67 @@
+#include "parse_tree.h"
+
+ParseTree::ParseTree(string name, int num, ...)
+{
+    va_list valist;
+    this->child = NULL;
+    this->next_sibling = NULL;
+    this->content = "";
+    this->name = name;
+    ParseTree *temp = NULL;
+    va_start(valist, num);
+    if (num > 0)
+    {
+        temp = va_arg(valist, ParseTree *);
+        this->child = temp;
+        this->line = temp->line;
+        //如果只有一个子节点
+        if (num == 1)
+        {
+            //如果是终结符
+            if (temp->content.size() > 0)
+            {
+                this->content = temp->content;
+            }
+            else
+            {
+                this->content = "";
+            }
+        }
+        //如果有不止一个子节点
+        else
+        {
+            for (int i = 0; i < num; i++)
+            {
+                temp->next_sibling = va_arg(valist, ParseTree *);
+                temp = temp->next_sibling;
+            }
+        }
+    }
+    else
+    {
+        int line = va_arg(valist, int);
+        this->line = line;
+        if (this->name == "CONSTANT_INT")
+        {
+            int value;
+            //8进制
+            if (strlen(yytext) > 1 && yytext[0] == '0' && yytext[1] != 'x' && yytext[1] != 'X')
+            {
+                sscanf(yytext, "%o", &value);
+            }
+            //16进制
+            else if (strlen(yytext) > 1 && yytext[0] == '0' && (yytext[1] == 'x' || yytext[1] == 'X'))
+            {
+                sscanf(yytext, "%x", &value);
+            }
+        }
+    }
+}
+
+ParseTree::ParseTree()
+{
+    child = NULL;
+    next_sibling = NULL;
+    content = "";
+    name = "";
+}
---
+++ parse_tree.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+
+using namespace std;
+
+extern char *yytext;
+extern int yylineo;
+
+class ParseTree
+{
+public:
+    string content;             //终结符内容
+    string name;                //符号名称
+    int line;                   //代码所在行数
+    ParseTree *child;           //子节点
+    ParseTree *next_sibling;    //兄弟节点
+    ParseTree(string name, int num, ...);
+    ParseTree();
+    ~ParseTree();
+};
