23509268070d016cdca48571edeb511e5cc61e42
MrZixi <lcx970715@gmail.com>
2019-05-26 14:10:41 +0800

    5.26 morning

--- praser.cpp
+++ praser.cpp
@@ -9,40 +9,161 @@ void Praser::init()
 {
     block first_block;
     blockStack.push_back(first_block);
+    op_math_map.insert("MUL_ASSIGN", "*");
+    op_math_map.insert("DIV_ASSIGN", "/");
+    op_math_map.insert("MOD_ASSIGN", "%");
+    op_math_map.insert("ADD_ASSIGN", "+");
+    op_math_map.insert("SUB_ASSIGN", "-");
+    op_math_map.insert("LEFT_ASSIGN", "<<");
+    op_math_map.insert("RIGHT_ASSIGN", ">>");
+    op_math_map.insert("AND_ASSIGN", "&");
+    op_math_map.insert("OR_ASSIGN", "|");
+    op_math_map.insert("XOR_ASSIGN", "^");
 }
 varNode Praser::praser_assignment_expression(ParseTree* assign_exp)
 {
-    
+    varNode node1, node2, node3;
+    if(assign_exp->child->name == "conditional_expression")
+    {
+        return praser_conditional_expression(assign_exp->child); 
+    }
+    //Truely is assignment expression
+    else if(assign_exp->child->name == "unary_expression")
+    {
+		ParseTree* unary_exp = assign_exp->child;
+		string op = assign_exp->child->next_sibling->child->name;
+		ParseTree* next_assign_exp = assign_exp->child->next_sibling->next_sibling;
+		node1 = praser_unary_expression(unary_exp);
+		node2 = praser_assignment_expression(next_assign_exp);
+		if (op == "=") 
+        {
+			node3 = node2;
+		}
+		else 
+        {
+			string tempname = "temp" + to_string(codePrinter.temp_var_count);
+			codePrinter.temp_var_count++;
+			node3 = createTempVar(tempname, node1.type);
+
+			blockStack.back()._var_map.insert({ tempname,node3 });
+            string op_math = op_math_map[op];
+            if(op_math == "*" || op_math == "/" || op_math == "+" || op_math == "-")
+            {
+                if (node1.type != node2.type) 
+                {
+					error(assign_exp->child->line, "Different type for two variables.");
+				}
+            }
+            else if(op_math == "&" || op_math == "|" || op_math == "^" || op_math == "<<" || op_math == ">>" || op_math == "%")
+            {
+                if ((node1.type != "int" && node1.type != "long" && node1.type != "long long") || (node2.type != "int" && node2.type != "long" && node2.type != "long long")) 
+                {
+					error(assign_exp->child->line, "The two variables must be integer.");
+				}
+            }
+            codePrinter.addCode(codePrinter.createCodeforVar(tempname, op_math, node1, node2));
+		}
+
+		codePrinter.addCode(codePrinter.createCodeforAssign(node1, node3));
+		return node1;
+	}
+    return node1;
+}
+varNode Praser::praser_conditional_expression(ParseTree* conditional_exp)
+{
+    if(conditional_exp->child->name == "logical_or_expression")
+    {
+        return praser_logical_or_expression(conditional_exp->child); 
+    }
 }
 varNode Praser::praser_logical_or_expression(ParseTree* logical_or_exp)
 {
+    varNode node1, node2;
+    if(logical_or_exp->child->name == "logical_and_expression")
+    {
+        return praser_logical_or_expression(logical_or_exp->child); 
+    }
+    else if(logical_or_exp->child->name == "logical_or_expression")
+    {
+        varNode node1 = praser_logical_or_expression(logical_or_exp->child);
+		varNode node2 = praser_logical_and_expression(logical_or_exp->child->next_sibling->next_sibling);
+        if(node1.type != "bool" || node2.type != "bool")
+        {
+            error(logical_or_exp->child->next_sibling->line, "Logical Or operation should only be used to bool-type variable.");
+        }
 
+        string tempname = "temp" + to_string(codePrinter.temp_var_count);
+		codePrinter.temp_var_count++;
+		varNode newnode = createTempVar(tempname, node1.type);
+
+		blockStack.back()._var_map.insert({tempname, newnode});
+		codePrinter.addCode(codePrinter.createCodeforVar(tempname, "||", node1, node2));
+
+		newnode.boolValue = codePrinter.getNodeName(node1) + " || " + codePrinter.getNodeName(node2);
+
+		return newnode;
+    }
+    return node1;
 }
 varNode Praser::praser_logical_and_expression(ParseTree* logical_and_exp)
 {
+    varNode node1, node2;
+    if(logical_and_exp->child->name == "inclusive_or_expression")
+    {
+        return praser_inclusive_or_expression(logical_and_exp->child); 
+    }
+    else if(logical_and_exp->child->name == "logical_and_expression")
+    {
+        varNode node1 = praser_logical_and_expression(logical_and_exp->child);
+		varNode node2 = praser_inclusive_or_expression(logical_and_exp->child->next_sibling->next_sibling);
+        if(node1.type != "bool" || node2.type != "bool")
+        {
+            error(logical_and_exp->child->next_sibling->line, "Logical Or operation should only be used to bool-type variable.");
+        }
 
+        string tempname = "temp" + to_string(codePrinter.temp_var_count);
+		codePrinter.temp_var_count++;
+		varNode newnode = createTempVar(tempname, node1.type);
+
+		blockStack.back()._var_map.insert({tempname, newnode});
+		codePrinter.addCode(codePrinter.createCodeforVar(tempname, "&&", node1, node2));
+
+		newnode.boolValue = codePrinter.getNodeName(node1) + " && " + codePrinter.getNodeName(node2);
+
+		return newnode;
+    }
+    return node1;
 }	
-varNode Praser::praser_inclusive_or_expression(ParseTree*)
+varNode Praser::praser_inclusive_or_expression(ParseTree* inclusive_or_exp)
+{
+     varNode node1, node2;
+    if(inclusive_or_exp->child->name == "exclusive_or_expression")
+    {
+        return praser_exclusive_or_expression(inclusive_or_exp->child); 
+    }
+    else
+    {
+        
+    }
+    return node1;
+}
+varNode Praser::praser_exclusive_or_expression(ParseTree* exclusive_or_exp)
 {
 
 }
-varNode Praser::praser_exclusive_or_expression(ParseTree*)
+varNode Praser::praser_and_expression(ParseTree* and_exp)
 {
 
 }
-varNode Praser::praser_and_expression(ParseTree*)
+varNode Praser::praser_equality_expression(ParseTree* equal_exp)
 {
 
 }
-varNode Praser::praser_equality_expression(ParseTree*)
+varNode Praser::praser_relational_expression(ParseTree* relational_exp)
 {
 
 }
-varNode Praser::praser_relational_expression(ParseTree*)
-{
-
-}
-varNode Praser::praser_shift_expression(ParseTree*)
+varNode Praser::praser_shift_expression(ParseTree* shift_exp)
 {
 
 }
@@ -93,7 +214,16 @@ bool Praser::lookup_var_in_current_block(string name)
 {
 
 }
-
+varNode Praser::createTempVar(string tempname, string temptype)
+{
+    varNode tempNode;
+    tempNode.name = tempname;
+    tempNode.type = temptype;
+    tempNode.count = -1;
+    tempNode.global_or_local = false;
+    tempNode.useAddress = false;
+    return tempNode;
+}
 void Praser::error(int line_number, string err_message)
 {
     cout<<"Error line:"<<line_number<<endl;
--- praser.h
+++ praser.h
@@ -15,6 +15,7 @@ class Praser{
     private:
         ParseTree* root;
         map<string, funcNode>func_map;
+        map<string, string>op_math_map;
         vector<block> blockStack;
         CodePrinter codePrinter;
         
@@ -32,7 +33,8 @@ class Praser{
 	    void praser_selection_statement(ParseTree*);
 	    void praser_iteration_statement(ParseTree*);
                                                          //arithmatic expressions:lcx       
-        varNode praser_assignment_expression(ParseTree*);			
+        varNode praser_assignment_expression(ParseTree*);		
+        varNode praser_conditional_expression(ParseTree*);	
 	    varNode praser_logical_or_expression(ParseTree*);			
 	    varNode praser_logical_and_expression(ParseTree*);	
 	    varNode praser_inclusive_or_expression(ParseTree*);
@@ -48,7 +50,7 @@ class Praser{
 
         
         bool lookup_var_in_current_block(string);
-
+        varNode createTempVar(string, string);
         void error(int, string);
         
     public:
